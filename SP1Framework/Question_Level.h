a1f1e47defaf70f9f0e4e1ed08a63f83ede311d4
Jean <lzyjeann@gmail.com>
2016-08-24 14:13:37 +0800

    Updated: Reduced 108 warnings to 0 warnings

--- SP1Framework/Question_Level.h
+++ SP1Framework/Question_Level.h
@@ -6,7 +6,6 @@ using namespace std;
 extern Console g_Console;
 extern struct SGameChar g_sChar;
 extern char map[50][150];
-extern enum EKEYS;
 extern bool printFog;
 extern bool toggleFog;
 
--- SP1Framework/Render_Story.h
+++ SP1Framework/Render_Story.h
@@ -3,13 +3,11 @@
 using namespace std;
 
 extern struct SGameChar g_sChar;
-extern enum EKEYS;
 extern double  g_dElapsedTime;
 extern double  g_dBounceTime;
 extern unsigned int mapSizeWidth;
 extern unsigned int mapSizeHeight;
 extern bool newMap;
-extern enum EGAMESTATES;
 extern int refreshMap;
 extern bool timeToWait;
 extern bool printHealth;
